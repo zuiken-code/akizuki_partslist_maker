68cce2c4afd3ab4433c5e925c14fba5c9c449446
zuiken-code <zuiken2022robo@gmail.com>
2024-12-15 22:35:01 +0900

    add get_url function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 def main():
+    shop = "秋月電子通商"    #今回は秋月電子限定(今後追加実装の可能性アリ)←結局しないやつ
     shop_id = "akizuki"     #akizuki,eleshop,sengoku が選択できる
     model_number = 116108   #秋月電子の販売コード
     model_count = 3         #何個買うのか
@@ -13,10 +14,13 @@ def main():
     subtotal = price * model_count
     price_str = "￥"+str(get_price(res))
     subtotal_str = "￥"+str(price * model_count)
+    shop = "秋月電子通商"
+    url = get_url(res)
     
     print(name)
     print(price_str)
     print(subtotal_str)
+    print(url)
 
 def get_res(shop_id,model_number):
     url = f"https://api.partscabi.net/v1/shop/{shop_id}/component/{model_number}/"
@@ -31,6 +35,10 @@ def get_name(res):
 def get_price(res):
     price = res.json()["prices"][0]["value"]
     return price
+
+def get_url(res):
+    url = res.json()["link"]
+    return url
 
 
 
